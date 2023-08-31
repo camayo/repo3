455e02f21c752c4be6fceef1f4f1c395a0a7d588
Cristian Amayo <camayo.sisteming@gmail.com>
2023-08-31 08:39:11 -0500

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,11 @@
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
+
+def messagingWin( String mess ) {
+    cmd "echo ${mess}"
+}
+
+def messagingLnx( String mess ) {
+    sh "echo ${mess}"
+}
